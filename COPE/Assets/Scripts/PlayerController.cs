bc39f5ddc6631409dd93cb557166ef3684d0dd46
bsilver3 <105662471+bsilver3@users.noreply.github.com>
2022-05-16 11:05:28 -0600

    Added PlayerController script

---
+++ COPE/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // cope
+    // brennan's script
+    // will add movement eventually
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
