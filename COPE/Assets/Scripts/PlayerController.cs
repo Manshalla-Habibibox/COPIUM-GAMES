3329ff6eae967abeccb40cd31542ae14ea50db36
bsilver3 <105662471+bsilver3@users.noreply.github.com>
2022-05-18 09:39:37 -0600

    basic variables added for testing

--- COPE/Assets/Scripts/PlayerController.cs
+++ COPE/Assets/Scripts/PlayerController.cs
@@ -8,10 +8,15 @@ public class PlayerController : MonoBehaviour
     // brennan's script
     // will add movement eventually
 
-    Vector3 movement;
-    Vector3 mouseMovement;
+    float mouseX;
+    float mouseY;
     float horizontalInput;
     float verticalInput;
+
+    float mouseSensitivity;
+
+    public Transform playerBody;
+    public Transform playerCam;
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +27,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        mouseX = Input.GetAxisRaw("Mouse X") * mouseSensitivity * Time.deltaTime;
+        mouseY = Input.GetAxisRaw("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        playerBody.Rotate(transform.up * mouseX);
     }
 }
