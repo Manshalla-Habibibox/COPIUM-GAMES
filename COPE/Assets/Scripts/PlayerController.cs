81e59ee56b88f24d148211edf11592efbe39d602
bsilver3 <105662471+bsilver3@users.noreply.github.com>
2022-05-18 10:41:14 -0600

    aa errors

--- COPE/Assets/Scripts/PlayerController.cs
+++ COPE/Assets/Scripts/PlayerController.cs
@@ -14,16 +14,12 @@ public class PlayerController : MonoBehaviour
 
     public CharacterController characterController;
 
-<<<<<<< Updated upstream
     public Transform groundCheck;
     public float groundCheckRadius = 0.4f;
     public LayerMask groundMask;
 
     Vector3 velocity;
     bool isGrounded;
-=======
-    Vector3 velocity;
->>>>>>> Stashed changes
 
     float gravity = -9.81f;
 
@@ -37,6 +33,11 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
 
         float x = Input.GetAxisRaw("Horizontal");
         float z = Input.GetAxisRaw("Vertical");
