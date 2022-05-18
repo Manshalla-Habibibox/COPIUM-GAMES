07301dbea8e0857501532e6f30aa3d4e47fe13fb
bsilver3 <105662471+bsilver3@users.noreply.github.com>
2022-05-18 10:20:27 -0600

    updated some movement

--- COPE/Assets/Scripts/PlayerController.cs
+++ COPE/Assets/Scripts/PlayerController.cs
@@ -17,7 +17,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
